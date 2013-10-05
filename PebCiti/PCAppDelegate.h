ae7f3d9aacb61c7e56ddd35542613b5fd92fb19e
Joe Masilotti <joseph.masilotti@gmail.com>
2013-10-05 12:37:12 -0400

    Cleans up directory structure.

--- PebCiti/PCAppDelegate.h
+++ PebCiti/PCAppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  PCAppDelegate.h
-//  PebCiti
-//
-//  Created by Joe Masilotti on 10/5/13.
-//  Copyright (c) 2013 Masilotti.com. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface PCAppDelegate : UIResponder <UIApplicationDelegate>
--- PebCiti/Supporting Files/main.m
+++ PebCiti/Supporting Files/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  PebCiti
-//
-//  Created by Joe Masilotti on 10/5/13.
-//  Copyright (c) 2013 Masilotti.com. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 #import "PCAppDelegate.h"
