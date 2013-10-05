a48ab78f7dc1c53f4ff8a6e941c5c3d636bf1b8a
Joe Masilotti <joseph.masilotti@gmail.com>
2013-10-05 12:34:24 -0400

    Initial Commit

---
+++ PebCiti/PCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PCAppDelegate.h
+//  PebCiti
+//
+//  Created by Joe Masilotti on 10/5/13.
+//  Copyright (c) 2013 Masilotti.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PebCiti/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PebCiti
+//
+//  Created by Joe Masilotti on 10/5/13.
+//  Copyright (c) 2013 Masilotti.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PCAppDelegate class]));
+    }
+}
