56f0135eda2d3d35fc80427fb6c519f63a4255d8
Joe Masilotti <joseph.masilotti@gmail.com>
2013-11-13 00:07:42 -0500

    Add TestFlight SDK.

--- PebCiti/PCAppDelegate.m
+++ PebCiti/PCAppDelegate.m
@@ -1,10 +1,15 @@
 #import "PCAppDelegate.h"
+#import "TestFlight.h"
 #import "PebCiti.h"
 
 @implementation PCAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+#ifndef DEBUG
+    [TestFlight setDeviceIdentifier:[UIDevice.currentDevice.identifierForVendor UUIDString]];
+    [TestFlight takeOff:@"8aaf96c4-077f-40ed-b336-10c1acadb047"];
+#endif
     [PebCiti.sharedInstance setUpAppearance];
     return YES;
 }
