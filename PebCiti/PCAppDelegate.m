dbb34d28a91642e20605a2fed2a18b5c42756fa9
Joe Masilotti <joseph.masilotti@gmail.com>
2013-11-24 19:04:50 -0500

    Add Crashlytics.

--- PebCiti/PCAppDelegate.m
+++ PebCiti/PCAppDelegate.m
@@ -1,3 +1,4 @@
+#import <Crashlytics/Crashlytics.h>
 #import "PCAppDelegate.h"
 #import "TestFlight.h"
 #import "PebCiti.h"
@@ -6,6 +7,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [Crashlytics startWithAPIKey:@"d719996ce2f7809259d6b116a1e5b1cf5d0f316d"];
 #ifndef DEBUG
     [TestFlight setDeviceIdentifier:[UIDevice.currentDevice.identifierForVendor UUIDString]];
     [TestFlight takeOff:@"8aaf96c4-077f-40ed-b336-10c1acadb047"];
